aaaa9c776b5a85e2fd7b14efb7629dad77c8dcff
Iosu <irecaldema14dw@ikzubirimanteo.com>
2015-05-13 14:24:23 +0200

    buscar

--- src/BuscarAlumno.java
+++ src/BuscarAlumno.java
@@ -1,4 +1,4 @@
-
+import com.zubiri.matriculas.Alumno;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
@@ -31,6 +31,73 @@ public class BuscarAlumno extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		
+		private static final String USUARIO = "root";
+		private static final String CONTRA = "zubiri";
+		static final String URL_BD = "jdbc:mysql://localhost/pruebas_java";
+		
+		System.out.println("Empieza buscando");
+		String referencia=request.getParameter("matricula");
+		
+		Connection con = null;	
+		Statement sentencia = null;		
+		try {
+			
+			// Register JDBC driver
+			Class.forName("com.mysql.jdbc.Driver");
+
+			// Open a connection
+			con = DriverManager.getConnection(URL_BD,USUARIO,CONTRA);
+			
+			sentencia = con.createStatement();
+			
+			String sql;		    
+			System.out.println("Referencia: "+referencia);		     
+			//sql="SELECT matricula, marca FROM coches WHERE matricula='"+referencia+"'";
+			sql="SELECT * FROM alumnos WHERE dni=\""+referencia+"\"";
+			
+			ResultSet buscar = sentencia.executeQuery(sql);
+			while (buscar.next()) {
+				/*matricula = buscar.getString("matricula");
+				marca = buscar.getString("marca");
+				motor = buscar.getBoolean("motor");
+				automatico = buscar.getBoolean("automatico");
+				n_ruedas = buscar.getInt("n_ruedas");
+				consumo = buscar.getInt("consumo");
+				System.out.println("Matrícula: "+matricula);
+				System.out.println("Marca: "+marca);*/
+				
+				response(response,Alumno alumno);
+			}
+			if (cont > 0) {
+				response(response,matricula,marca,motor,automatico,n_ruedas,consumo);
+			} else {
+				response(response, "No se encontró el vehículo");
+			}
+			con.close();    
+		
+		} catch(ArrayIndexOutOfBoundsException e) {
+			//response(response, "no se encontro el vehiculo");
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		// Respuesta simple
+		private void response(HttpServletResponse response,String msg) throws IOException {
+			response.setContentType( "text/html; charset=iso-8859-1" );
+			PrintWriter out = response.getWriter();
+			out.println("<html>");
+			out.println("<head>");
+				out.println("<title> Respuesta </title>");
+				out.println("<link rel='stylesheet' type='text/css' href='stylebd.css'>");
+			out.println("</head>");
+			out.println("<body>");				
+			out.println("<p>" + msg + "</p>");
+			out.println("<a href='index.html'> <button> Volver </button> </a>");
+			out.println("</body>");
+			out.println("</html>");
+		}
+		
 	}
 
 }
