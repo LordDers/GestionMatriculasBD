5333f7b14c73fc237174759351dee8c210975900
Iosu <irecaldema14dw@ikzubirimanteo.com>
2015-05-15 13:28:59 +0200

    insertar

--- src/Insertar.java
+++ src/Insertar.java
@@ -3,14 +3,13 @@ import com.zubiri.matriculas.Alumno;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.Statement;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import com.zubiri.matriculas.Alumno;
 
 /**
  * Servlet implementation class Matricular
